ae0d032cc2f641e2f5458d8bcb5ee8f3c0276959
roman_krat <kratroma17@gmail.com>
2020-05-21 16:04:07 +0300

    remove redirect

--- appleLogin.go
+++ appleLogin.go
@@ -18,7 +18,6 @@ import (
 type AppleConfig struct {
 	TeamID      string      //Your Apple Team ID
 	ClientID    string      //Your Service which enable sign-in-with-apple service
-	RedirectURI string      //Your RedirectURI config in apple website
 	KeyID       string      //Your Secret Key ID
 	AESCert     interface{} //Your Secret Key Created By X509 package
 }
@@ -32,8 +31,11 @@ type AppleAuthToken struct {
 	TokenType    string `json:"token_type"`    //Token Type
 }
 
-const AppleAuthURL = "https://appleid.apple.com/auth/token" //the auth URL of apple
-const AppleGrantType = "authorization_code"                 //the grant type of apple auth
+const (
+	AppleAuthURL   = "https://appleid.apple.com/auth/token" //the auth URL of apple
+	AppleGrantType = "authorization_code"                   //the grant type of apple auth
+	AUDLink = "https://appleid.apple.com" //aud field
+)
 
 //LoadP8CertByByte use x509.ParsePKCS8PrivateKey to Parse cert file
 func (a *AppleConfig) LoadP8CertByByte(str []byte) (err error) {
@@ -56,26 +58,15 @@ func (a *AppleConfig) LoadP8CertByFile(path string) (err error) {
 }
 
 //InitAppleConfig init a new Client of this pkg
-func InitAppleConfig(teamID string, clientID string, redirectURI string, keyID string) *AppleConfig {
+func InitAppleConfig(teamID string, clientID string, keyID string) *AppleConfig {
 	return &AppleConfig{
 		teamID,
 		clientID,
-		redirectURI,
 		keyID,
 		nil,
 	}
 }
 
-//CreateCallbackURL create a callback URL for frontend
-func (a *AppleConfig) CreateCallbackURL(state string) string {
-	u := url.Values{}
-	u.Add("response_type", "code")
-	u.Add("redirect_uri", a.RedirectURI)
-	u.Add("client_id", a.ClientID)
-	u.Add("state", state)
-	u.Add("scope", "name email")
-	return "https://appleid.apple.com/auth/authorize?" + u.Encode()
-}
 
 //input your code and expire-time to get AccessToken of apple
 func (a *AppleConfig) GetAppleToken(code string, expireTime int64) (*AppleAuthToken, error) {
@@ -88,7 +79,7 @@ func (a *AppleConfig) GetAppleToken(code string, expireTime int64) (*AppleAuthTo
 		"iss": a.TeamID,
 		"iat": time.Now().Unix(),
 		"exp": time.Now().Unix() + expireTime,
-		"aud": "https://appleid.apple.com",
+		"aud":AUDLink ,
 		"sub": a.ClientID,
 	})
 	//set JWT header
@@ -103,10 +94,9 @@ func (a *AppleConfig) GetAppleToken(code string, expireTime int64) (*AppleAuthTo
 	v.Set("client_secret", tokenString)
 	v.Set("code", code)
 	v.Set("grant_type", AppleGrantType)
-	v.Set("redirect_uri", a.RedirectURI)
 	vs := v.Encode()
 	//send request
-	resp, body, err2 := gorequest.New().Post("https://appleid.apple.com/auth/token").Type("urlencoded").Send(vs).End()
+	resp, body, err2 := gorequest.New().Post(AppleAuthURL).Type("urlencoded").Send(vs).End()
 	if err2 != nil {
 		return nil, fmt.Errorf(fmt.Sprint(err2))
 	}
