b9e98b29ef6c8e12eb39932962672299b616ee36
roman_krat <kratroma17@gmail.com>
2020-05-22 14:42:14 +0300

    add method DecodeToken

--- appleLogin.go
+++ appleLogin.go
@@ -16,10 +16,10 @@ import (
 
 // AppleConfig Main struct of the package
 type AppleConfig struct {
-	TeamID      string      //Your Apple Team ID
-	ClientID    string      //Your Service which enable sign-in-with-apple service
-	KeyID       string      //Your Secret Key ID
-	AESCert     interface{} //Your Secret Key Created By X509 package
+	TeamID   string      //Your Apple Team ID
+	ClientID string      //Your Service which enable sign-in-with-apple service
+	KeyID    string      //Your Secret Key ID
+	AESCert  interface{} //Your Secret Key Created By X509 package
 }
 
 // AppleAuthToken main response of apple REST-API
@@ -31,15 +31,20 @@ type AppleAuthToken struct {
 	TokenType    string `json:"token_type"`    //Token Type
 }
 
+type AppleUser struct {
+	ID    string `json:"sub,omitempty"`
+	Email string `json:"email,omitempty"`
+}
+
 const (
 	AppleAuthURL   = "https://appleid.apple.com/auth/token" //the auth URL of apple
 	AppleGrantType = "authorization_code"                   //the grant type of apple auth
-	AUDLink = "https://appleid.apple.com" //aud field
+	AUDLink        = "https://appleid.apple.com"            //aud field
 )
 
 //LoadP8CertByByte use x509.ParsePKCS8PrivateKey to Parse cert file
 func (a *AppleConfig) LoadP8CertByByte(str []byte) (err error) {
-	block, _ := pem.Decode([]byte(str))
+	block, _ := pem.Decode(str)
 	cert, err := x509.ParsePKCS8PrivateKey(block.Bytes)
 	if err != nil {
 		return
@@ -54,7 +59,7 @@ func (a *AppleConfig) LoadP8CertByFile(path string) (err error) {
 	if err != nil {
 		return
 	}
-	return a.LoadP8CertByByte([]byte(b))
+	return a.LoadP8CertByByte(b)
 }
 
 //InitAppleConfig init a new Client of this pkg
@@ -67,7 +72,6 @@ func InitAppleConfig(teamID string, clientID string, keyID string) *AppleConfig 
 	}
 }
 
-
 //input your code and expire-time to get AccessToken of apple
 func (a *AppleConfig) GetAppleToken(code string, expireTime int64) (*AppleAuthToken, error) {
 	//test cert
@@ -79,7 +83,7 @@ func (a *AppleConfig) GetAppleToken(code string, expireTime int64) (*AppleAuthTo
 		"iss": a.TeamID,
 		"iat": time.Now().Unix(),
 		"exp": time.Now().Unix() + expireTime,
-		"aud":AUDLink ,
+		"aud": AUDLink,
 		"sub": a.ClientID,
 	})
 	//set JWT header
@@ -112,4 +116,31 @@ func (a *AppleConfig) GetAppleToken(code string, expireTime int64) (*AppleAuthTo
 		return nil, err
 	}
 	return t, nil
+}
+
+func (a *AppleConfig) DecodeToken(token string) (*AppleUser, error) {
+	t, err := jwt.Parse(token, func(token *jwt.Token) (interface{}, error) {
+		return nil, nil
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	claims, ok := t.Claims.(jwt.MapClaims)
+
+	if !ok {
+		return nil, errors.New("fail decode")
+	}
+
+	u := &AppleUser{}
+
+	if v, ok := claims["email"]; ok {
+		u.Email = fmt.Sprintf("%v", v)
+	}
+
+	if v, ok := claims["sub"]; ok {
+		u.ID = fmt.Sprintf("%v", v)
+	}
+
+	return u, nil
 }
